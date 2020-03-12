943ae48090fe8d6cc15fe804d8e132e217fdec4b
vijay <vijaysm97@gmail.com>
2020-03-12 15:06:55 +0530

    theme.js is added

---
+++ src/utils/theme.js
@@ -0,0 +1,12 @@
+const Theme = {
+    colors: {
+        main: '#003459',
+        mainDark: "#00171f",
+        mainLight: '#007EA7',
+        mainLighter: '#00A8E8',
+        textColor: '#333',
+        whiteColor: '#FFF',
+        shadow: 'rgba(0,0,0,.2)'
+    },
+
+}
