279afcb639b0cf32f486a4e7cd0e1f2fc18f4a16
vijay <vijaysm97@gmail.com>
2020-04-10 15:46:31 +0530

    Updates

---
+++ precache-manifest.d6d205bef06aae57bc80f3f5bd1fae97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a2182310320cfaee7ae08c419e9a241",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "fa4d49ae966c2ee401b0",
+    "url": "/ToDoList/static/js/2.5480238c.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/ToDoList/static/js/2.5480238c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21df3a76ba06e8b188d5",
+    "url": "/ToDoList/static/js/main.44a72867.chunk.js"
+  },
+  {
+    "revision": "f37b1182598fcca5c5a9",
+    "url": "/ToDoList/static/js/runtime-main.e2297747.js"
+  }
+]);
